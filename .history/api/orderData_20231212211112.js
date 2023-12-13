f7f675b7a557194a9b81e72fcc9fc46a42e3d9e7
jonathan slater <jonslater2005@gmail.com>
2023-12-12 21:20:22 -0600

    promises written in orderdata.js

---
+++ .history/api/orderData_20231212211112.js
@@ -0,0 +1,15 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+const getOrder = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
