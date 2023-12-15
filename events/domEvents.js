f667db6b95d99c8f81218fe3ffd56c8b70739a20
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2023-12-14 21:54:18 -0600

    Fixed navBar, commented out startApp unused, made div data for close order and create items

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,12 +1,14 @@
-// import getOrderCustomerData from '../api/mergedData';
-// import { viewOrder } from '../pages/viewOrders';
+import getOrderCustomerData from '../api/mergedData';
+import { viewOrder } from '../pages/viewOrders';
+// import closeOrder from '../pages/closeOrder';
+// import createEditItem from '../pages/createItems';
 
-// const domEvents = () => {
-//   document.querySelector('#view').addEventListener('click', (e) => {
-//     if (e.target.id.includes('view-order-btn')) {
-//       const [, firebaseKey] = e.target.id.split('--');
-//       getOrderCustomerData(firebaseKey).then(viewOrder);
-//     }
-//   });
-// };
-// export default domEvents;
+const domEvents = () => {
+  document.querySelector('#view').addEventListener('click', (e) => {
+    if (e.target.id.includes('view-order-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      getOrderCustomerData(firebaseKey).then(viewOrder);
+    }
+  });
+};
+export default domEvents;
