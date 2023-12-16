9ca53a2596d750a7cc2eebd041568f3b02166264
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2023-12-16 11:19:49 -0600

    deleted view items button

--- pages/createItems.js
+++ pages/createItems.js
@@ -3,16 +3,21 @@ import renderToDOM from '../utils/renderToDom';
 
 const createEditItem = (obj = {}) => {
   clearDom();
-  const domString = `<div>
-  <div class="form-group">
-  <label for="price">Item Name</label>
-  <input type="text" class="form-control" id="item_name" placeholder="Item Name" value="${obj.price || ''}" required>
-</div>
-<div class="form-group">
+  const domString = `
+  <form id="${obj.firebaseKey ? `update-item--${obj.firebaseKey}` : 'submit-item'}" class="mb-4">
+    <div>
+      <div class="form-group">
+        <label for="price">Item Name</label>
+        <input type="text" class="form-control" id="item_name" placeholder="Item Name" value="${obj.price || ''}" required>
+      </div>
+      <div class="form-group">
         <label for="price">Item Price</label>
         <input type="text" class="form-control" id="price" placeholder="Item Price" value="${obj.price || ''}" required>
       </div>
-  </div>`;
+    </div>
+    <button type="submit" class="btn btn-primary">Submit Item
+    </button>
+  </form>`;
   renderToDOM('#form-container', domString);
 };
 
