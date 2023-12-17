389cf28c181edd347be598a45deebc6c1cade2c3
jonathan slater <jonslater2005@gmail.com>
2023-12-17 14:47:26 -0600

    Cards populating with customer and order information

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,9 +1,6 @@
 import { getAllCustomers } from './customerData';
 import { getAllOrders } from './orderData';
-// TODO#1 get all orders
-// TODO#2 get all customers
-// TODO#3 filter by customer_id
-// TODO#4 Append data to create new object
+
 const getOrderByCustomerData = () => new Promise((resolve, reject) => {
   getAllCustomers().then((orderObjects) => {
     getAllOrders()
--- pages/Orders.js
+++ pages/Orders.js
@@ -1,29 +1,34 @@
+import { getSingleCustomer } from '../api/customerData';
 import clearDom from '../utils/clearDom';
 import renderToDOM from '../utils/renderToDom';
-import getOrderByCustomerData from '../api/mergedData';
 
 const showOrders = (array) => {
-  getOrderByCustomerData();
-  console.warn(array);
   clearDom();
+  const renderAllOrderCards = (domStringTwo) => {
+    renderToDOM('#view', domStringTwo);
+  };
 
   let domString = '';
-  array.forEach((item) => {
-    domString += `<div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">${item.order_name}</h5>
-      <h6 class="card-subtitle mb-2 text-body-secondary">Status: ${item.order_status}</h6>
-      <p class="card-text">Phone: ${item.first_name} </p>
-      <p class="card-text">Email:</p>
-      <p class="card-text"> In person/Phone : ${item.order_type}</p>
-      <a href="#" class="card-link">Delete</a>
-      <a href="#" class="card-link">Edit</a>
-      <a href="#" class="card-link">Details</a>
-      <button id="view_order" type="button" class="btn btn-danger">Danger</button>
-    </div>
-  </div>`;
+  array.forEach((order) => {
+    getSingleCustomer(order.customer_id).then((customer) => {
+      domString
+          += `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${order.order_name}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Status: ${order.order_status}</h6>
+          <p class="card-text">Phone: ${customer.first_name} </p>
+          <p class="card-text">Email:</p>
+          <p class="card-text"> In person/Phone : ${order.order_type}</p>
+          <a href="#" class="card-link">Delete</a>
+          <a href="#" class="card-link">Edit</a>
+          <a href="#" class="card-link">Details</a>
+          <button id="view_order" type="button" class="btn btn-danger">Danger</button>
+        </div>
+      </div>`;
+      renderAllOrderCards(domString);
+    });
+    // renderToDOM('#store', domString);
   });
-  renderToDOM('#store', domString);
 };
 
 export default showOrders;
