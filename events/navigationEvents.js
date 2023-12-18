cbc2c0bc24bbbc3f6fb0e49492f5be7f71084f15
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2023-12-17 18:41:30 -0600

    view all orders button works

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,11 +1,11 @@
-import { getOrder } from '../api/orderData';
-import { viewOrder } from '../pages/viewOrders';
+import { getAllOrders } from '../api/orderData';
 import { signOut } from '../utils/auth';
+import showOrders from '../pages/Orders';
 
 const navigationEvents = () => {
   document.querySelector('#logout-button').addEventListener('click', signOut);
   document.querySelector('#allOrders').addEventListener('click', () => {
-    getOrder().then(viewOrder);
+    getAllOrders().then(showOrders);
   });
 };
 
--- utils/startApp.js
+++ utils/startApp.js
@@ -3,14 +3,9 @@ import domBuilder from '../shared/domBuilder';
 import navBar from '../shared/navBar';
 import domEvents from '../events/domEvents';
 import formEvents from '../events/formEvents';
-<<<<<<< HEAD
 import navigationEvents from '../events/navigationEvents';
-// import { getOrder } from '../api/orderData';
-=======
-// import navigationEvents from '../events/navigationEvents';
 import showOrders from '../pages/Orders';
 import { getAllOrders } from '../api/orderData';
->>>>>>> 4fd2e1cf5adf61011834b9285ecdb9fbab6b4b00
 
 const startApp = () => {
   domBuilder();
