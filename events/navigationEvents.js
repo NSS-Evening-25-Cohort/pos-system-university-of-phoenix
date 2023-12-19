bd6eb3a8d6dc7de95c21a294713bbfcbb301f985
Tion Blackmon <tionblackmon@icloud.com>
2023-12-18 20:23:14 -0600

    Correction - added eevent listeners in the wrong folder. Meant to add it in domEvens.js

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -9,21 +9,6 @@ const navigationEvents = () => {
     getAllOrders().then(showOrders);
   });
   document.querySelector('#logo_btn').addEventListener('click', homeScreen);
-
-  // document.querySelector('#edit-item-btn').addEventListener('click', () => {
-  //   // TODO: Will need PATCH.
-  // });
-  // TODO: Will revisit this
-  // if (e.target.id.includes('Delete Item')) {
-  //   // eslint-disable-next-line no-alert
-  //   if (window.confirm('Want to delete?')) {
-  //     const [, firebaseKey] = e.target.id.split('--');
-  //     // TODO: Will need itemData to continue with this. deleteItem() doesn't exist yet.
-  //     deleteItem(firebaseKey).then(() => {
-  //       getAllOrders().then(showOrders);
-  //     });
-  //   }
-  // }
 };
 
 export default navigationEvents;
