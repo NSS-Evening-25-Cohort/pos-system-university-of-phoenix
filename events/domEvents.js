9f77b09fb0a03db68037e0c83363fc33dc2906a4
jonathan slater <jonslater2005@gmail.com>
2023-12-18 20:08:33 -0600

    working on form

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,4 +1,5 @@
 import { getAllOrders } from '../api/orderData';
+import editOrderForm from '../forms.js/creatEditOrder';
 import showOrders from '../pages/Orders';
 // import closeOrder from '../pages/closeOrder';
 // import createEditItem from '../pages/createItems';
@@ -7,6 +8,10 @@ const domEvents = () => {
   document.querySelector('#store').addEventListener('click', (e) => {
     if (e.target.id.includes('allOrders')) {
       getAllOrders().then(showOrders);
+    }
+    if (e.target.id.includes('newOrder')) {
+      console.warn('clicky');
+      editOrderForm();
     }
   });
 };
