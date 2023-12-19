01a90bdb9aca73da04830fe6619897ebcc1c1d40
jonathan slater <jonslater2005@gmail.com>
2023-12-18 22:52:24 -0600

    update to item cards

--- pages/orderDetails.js
+++ pages/orderDetails.js
@@ -1,44 +1,29 @@
 import clearDom from '../utils/clearDom';
 import renderToDOM from '../utils/renderToDom';
 
-<<<<<<< HEAD
 const showItems = (array) => {
-=======
-const emptyItems = () => {
-  const domString = '<h1>!!!No Items!!!</h1>';
-  renderToDOM('#main-container', domString);
-};
-
-const showItems = (item) => {
->>>>>>> b50d95f26a2bea7dfe5692ecf3e246707df83bae
   clearDom();
   const renderAllItemCards = (domStringTwo) => {
     renderToDOM('#view', domStringTwo);
   };
 
   let domString = '';
-  array.forEach((item) => {
+  array.forEach((order) => {
     domString
-      += `
-      <div class="card">
-        <div class="card-body" style="height: 180px;">
-          <h5 class="card-title">${item.item_name}</h5>
-<<<<<<< HEAD
-          <h3>${item.price}</h3>
-=======
-          <h3>${item.item_price}</h3>
->>>>>>> b50d95f26a2bea7dfe5692ecf3e246707df83bae
-            <hr>
-            <i id="edit-item-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info">Edit Item</i>
-            <i id="delete-item-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt">Delete Item</i>
+          += `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title"></h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
+          <p class="card-text"> ${order.item_name} </p>
+          <p class="card-text"> ${order.item_price}</p>
+          <p class="card-text">  </p>
+          <button type="button" id="delete-order" class="btn btn-danger">Delete</button>
+          <button type="button" id="edit-order" class="btn btn-success">Edit</button>
+          <button type="button" id="details-item" class="btn-info"btn >Details</button>
         </div>
       </div>`;
-<<<<<<< HEAD
     renderAllItemCards(domString);
   });
-=======
-  renderToDOM('#main-container', domString);
->>>>>>> b50d95f26a2bea7dfe5692ecf3e246707df83bae
 };
 // this is fuctioning
 
