9b24e5e3b368a4458d8957001d079224bbc6a8e4
jonathan slater <jonslater2005@gmail.com>
2023-12-18 21:16:41 -0600

    adding form event data

--- forms.js/creatEditOrder.js
+++ forms.js/creatEditOrder.js
@@ -1,24 +1,27 @@
 import clearDom from '../utils/clearDom';
 import renderToDOM from '../utils/renderToDom';
 
-const editOrderForm = () => {
+const editOrderForm = (obj = {}) => {
   clearDom();
   const domString = `
-    <form id="editOrderForm" class="mb-4">
-      <div class="form-group">
-        <label for="image">First Name</label>
-        <input type="text" class="form-control" id="first_name" placeholder="First Name" required>
-      </div>
-      <div class="form-group">
-        <label for="image">Last Name</label>
-        <input type="text" class="form-control" id="last_name" placeholder="Last Name" required>
-      </div>
-      <div class="form-group">
-        <label for="title">Email</label>
-        <input type="email" class="form-control" id="email" aria-describedby="Email" placeholder="Enter Email" required>
-      </div>
-      <button type="submit" class="btn btn-primary mt-3">Submit Author</button>
-    </form>`;
+  <form id="${obj.firebaseKey ? `update-item--${obj.firebaseKey}` : 'submit-item'}" class="mb-4">
+  <div class="form-group">
+    <label for="title">Order Name</label>
+    <input type="text" class="form-control" id="title" aria-describedby="Order Name" placeholder="Enter Item" value="${obj.name || ''}" required>
+  </div>
+  <div class="form-group">
+  <label for="price">Price</label>
+  <input type="text" class="form-control" id="price" placeholder="Customer Phone" value="${obj.customer_phone || ''}" required>
+</div>
+  <div class="form-group">
+    <label for="price">Price</label>
+    <input type="text" class="form-control" id="price" placeholder="Customer Email" value="${obj.Customer_email || ''}" required>
+  </div>
+  <div class="form-group" id="select-item">
+  </div>
+  <button type="submit" class="btn btn-primary">Add/Edit Item
+  </button>
+</form>`;
 
   renderToDOM('#form-container', domString);
 };
